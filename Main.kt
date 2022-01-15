9352551c3d61caf8a4d7bc0d9c6ca268dfa86eee
fvn70 <fvn_reg@mail.ru>
2022-01-15 20:30:59 +0700

    Minesweeper. Stage 2/5: Flexible mines

--- Main.kt
+++ Main.kt
@@ -2,13 +2,40 @@ package minesweeper
 
 import kotlin.random.Random
 
+
+class MineField(val rows: Int, val cols: Int) {
+    val arr = List(rows) { MutableList(cols) { '.' } }
+
+    fun setMines(n: Int) {
+        var cnt = 0
+        while (cnt < n) {
+            val r = Random.nextInt(rows)
+            val c = Random.nextInt(cols)
+            if (arr[r][c] != 'X') {
+                arr[r][c] = 'X'
+                cnt++
+            }
+        }
+    }
+
+    fun draw() {
+        for (i in 0..rows - 1) {
+            for (j in 0..cols - 1) {
+                print(arr[i][j])
+            }
+            println()
+        }
+    }
+}
+
 fun main() {
     val rows = 9
     val cols = 9
-    print("How many mines do you want on the field? >")
+    val game = MineField(rows, cols)
+
+    print("How many mines do you want on the field? ")
     val mines = readLine()!!.toInt()
 
-    val mineField = MineField(rows, cols, mines)
-    mineField.game()
+    game.setMines(mines)
+    game.draw()
 }
-
