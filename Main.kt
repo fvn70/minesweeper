9c1fc91ef34eccfab927f9b1ba3487105777def9
fvn70 <fvn_reg@mail.ru>
2022-01-16 11:47:31 +0700

    Minesweeper. Stage 3/5: Look around you

--- Main.kt
+++ Main.kt
@@ -3,24 +3,49 @@ package minesweeper
 import kotlin.random.Random
 
 
-class MineField(val rows: Int, val cols: Int) {
+class MineField(val rows: Int, val cols: Int, val n: Int) {
     val arr = List(rows) { MutableList(cols) { '.' } }
+        init {
+            var cnt = 0
+            while (cnt < n) {
+                val r = Random.nextInt(rows)
+                val c = Random.nextInt(cols)
+                if (arr[r][c] != 'X') {
+                    arr[r][c] = 'X'
+                    cnt++
+                }
+            }
+            cntMines()
+        }
 
-    fun setMines(n: Int) {
-        var cnt = 0
-        while (cnt < n) {
-            val r = Random.nextInt(rows)
-            val c = Random.nextInt(cols)
-            if (arr[r][c] != 'X') {
-                arr[r][c] = 'X'
-                cnt++
+    fun cntMines() {
+        for (i in 0 until rows) {
+            for (j in 0 until cols) {
+                if (arr[i][j] == '.') {
+                    val n = cntNeighbors(i, j)
+                    if (n > 0) {
+                        arr[i][j] = n.digitToChar()
+                    }
+                }
             }
         }
     }
 
+    fun cntNeighbors(r: Int, c: Int): Int {
+        var cnt = 0
+        for (i in r - 1..r + 1) {
+            for (j in c - 1..c + 1) {
+                if (i in 0 until rows && j in 0 until cols) {
+                    cnt += if (arr[i][j] == 'X') 1 else 0
+                }
+            }
+        }
+        return cnt
+    }
+
     fun draw() {
-        for (i in 0..rows - 1) {
-            for (j in 0..cols - 1) {
+        for (i in 0 until rows) {
+            for (j in 0 until cols) {
                 print(arr[i][j])
             }
             println()
@@ -31,11 +56,10 @@ class MineField(val rows: Int, val cols: Int) {
 fun main() {
     val rows = 9
     val cols = 9
-    val game = MineField(rows, cols)
 
     print("How many mines do you want on the field? ")
     val mines = readLine()!!.toInt()
 
-    game.setMines(mines)
+    val game = MineField(rows, cols, mines)
     game.draw()
 }
