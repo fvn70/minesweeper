dd0641431f23bc16e0be93ed393c669c628cb8c3
fvn70 <fvn_reg@mail.ru>
2022-01-16 17:29:01 +0700

    Stage 4/5: Prepare for battle

--- Main.kt
+++ Main.kt
@@ -5,6 +5,9 @@ import kotlin.random.Random
 
 class MineField(val rows: Int, val cols: Int, val n: Int) {
     val arr = List(rows) { MutableList(cols) { '.' } }
+    val marks = List(rows) { MutableList(cols) { false } }
+    var find = 0
+    var stars = 0
         init {
             var cnt = 0
             while (cnt < n) {
@@ -44,11 +47,43 @@ class MineField(val rows: Int, val cols: Int, val n: Int) {
     }
 
     fun draw() {
+        println("\n │123456789│")
+        println("—│—————————│")
         for (i in 0 until rows) {
+            print("${i + 1}│")
             for (j in 0 until cols) {
-                print(arr[i][j])
+                print(if (marks[i][j]) '*' else if (arr[i][j] == 'X') '.' else arr[i][j])
             }
-            println()
+            println("│")
+        }
+        println("—│—————————│")
+    }
+
+    fun setMark(r: Int, c: Int): Boolean {
+        if (arr[r][c] in '1'..'8') {
+            println("There is a number here!")
+            return false
+        } else {
+            marks[r][c] = !marks[r][c]
+            stars += if (marks[r][c]) 1 else -1
+            if (arr[r][c] == 'X') {
+                find += if (marks[r][c]) 1 else -1
+            }
+        }
+        return true
+    }
+
+    fun game() {
+        draw()
+        while (true) {
+            print("Set/delete mines marks (x and y coordinates): ")
+            val (x, y) = readLine()!!.split(" ").map { it.toInt() }
+            if (!setMark(y - 1, x - 1)) continue
+            draw()
+            if (find == n && stars == n) {
+                println("Congratulations! You found all the mines!")
+                break
+            }
         }
     }
 }
@@ -60,6 +95,5 @@ fun main() {
     print("How many mines do you want on the field? ")
     val mines = readLine()!!.toInt()
 
-    val game = MineField(rows, cols, mines)
-    game.draw()
+    MineField(rows, cols, mines).game()
 }
